5d367c55261c0a2d71412c00b04cb46367a12b1e
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-14 13:22:37 -0500

    feat(problems): solve 02 identical trees

--- src/02_identical_trees.py
+++ src/02_identical_trees.py
@@ -4,5 +4,10 @@ from tree import TreeNode
 
 
 def is_identical(a: TreeNode | None, b: TreeNode | None) -> bool:
-    """Same structure and same values."""
-    raise NotImplementedError
+    if not a and not b:
+        return True
+
+    if (not a or not b) or a.value != b.value:
+        return False
+
+    return is_identical(a.left, b.left) and is_identical(a.right, b.right)
