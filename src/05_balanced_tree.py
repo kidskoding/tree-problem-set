4a9ff8133d8ccfd2eb7dd87597123e43fcea064a
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-09-14 13:46:57 -0500

    added prob 05

--- src/05_balanced_tree.py
+++ src/05_balanced_tree.py
@@ -4,5 +4,30 @@ from tree import TreeNode
 
 
 def is_balanced(root: TreeNode | None) -> bool:
-    """Height difference of left/right subtrees <= 1 at every node."""
-    raise NotImplementedError
+    def height(node: TreeNode | None) -> int:
+        if not node:
+            return -1
+
+        return max(height(node.left), height(node.right))
+
+    if not root or abs(height(root.left) - height(root.right)) > 1:
+        return False
+
+    return is_balanced(root.left) and is_balanced(root.right)
+
+
+# alternative solution!
+def is_balanced_alternate(root: TreeNode | None) -> bool:
+    def is_balanced_alternate_helper(root: TreeNode | None) -> int:
+        if not root:
+            return 0
+
+            leftHeight = is_balanced_alternate_helper(root.left)
+            rightHeight = is_balanced_alternate_helper(root.right)
+
+            if leftHeight == -1 or rightHeight == -1 or \
+               abs(leftHeight - rightHeight) > 1:
+
+                return -1
+
+    return is_balanced_alternate_helper(root) > 0
